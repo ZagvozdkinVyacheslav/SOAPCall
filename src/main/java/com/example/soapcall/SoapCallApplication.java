490b68cfd5370e309d802a96bf2342e308873e93
zagvo <zagvozdkin12@gmail.com>
2023-09-18 12:46:59 +0300

    start

---
+++ src/main/java/com/example/soapcall/SoapCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soapcall/SoapCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
